804f9eab1dc89c9701fff8a526bfa930d08dffab
Bennett Sampson <sampson246156@gmail.com>
2022-11-02 16:32:15 -0600

    Put in proper GitHub repo link

--- src/components/MyGitHub.jsx
+++ src/components/MyGitHub.jsx
@@ -1,7 +1,7 @@
 const MyGitHub = () => {
     return (
         <p>
-            Link to GitHub: <a href="https://github.com/dldf/reactdemo"> https://github.com/dldf/reactdemo</a>
+            Link to GitHub: <a href="https://github.com/bennett-sampson/CSCI331Lab9"> https://github.com/bennett-sampson/CSCI331Lab9</a>
         </p>
     )
 }
